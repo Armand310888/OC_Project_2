a3ca607124f503eee2e595505a267018038f3c93
Armand de la Porte des Vaux <armand.delaporte@gmail.com>
2026-02-16 20:31:34 +0100

    Phase 3 feat: phase 3 initial implementation ready for testing

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ The implementation evolves through multiple phases:
 - Phase 4: Download and save product images from each product page
 
 """
-# PHASE 2: EXTRACT DATA FROM ALL PRODUCT IN A GIVEN CATEGORY
+# PHASE 3: EXTRACT DATA FROM EVERY PRODUCT FROM EVERY CATEGORIES
 
 # Third-party libraries
 import requests
@@ -29,6 +29,13 @@ def extract_table_value(product_parsed_html, label):
     row = th.parent
     return row.find("td").get_text(strip=True)
 
+# Helper to retieve and parse HTML content from URL
+def html_parser(url):
+    response = requests.get(url)
+    raw_html = response.text
+    return BeautifulSoup(raw_html, "html.parser")
+
+# Create a list of currencies
 CURRENCIES = ["£", "€", "$"]
 
 # Define a fixed column order for the CSV
@@ -45,50 +52,66 @@ fieldnames = [
     "image_url",
 ]
 
-# Open the CSV, write the header, then write one row per product
-with open("book_data.csv", "w", encoding="utf-8", newline="") as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+# Dowload and parse "Books to Scrape" website homepage
+homepage_url = "https://books.toscrape.com/"
+homepage_parsed = html_parser(homepage_url)
+
+# Extracting categories absolute URLs
+categories_parent = homepage_parsed.find("div", class_="side_categories")
+categories_content = categories_parent.find_all("a")
+categories_absolute_urls = [
+    urljoin(homepage_url, relative_url["href"])
+    for relative_url in categories_content
+]
+categories_absolute_urls = []
+for a in categories_content:
+    name = a.get_text(strip=True)
+    href = a.get("href")
+    if name == "Books":
+        continue
+    categories_absolute_urls.append(urljoin(homepage_url, href))
+
+# Download and parse every category page
+for category_absolute_url in categories_absolute_urls:
+    category_parsed = html_parser(category_absolute_url)
+
+    # Extract category name
+    category_name = (
+        category_parsed
+        .find("li", class_="active")
+        .get_text(strip=True)
+    )
+
+    # Create a specific csv file per catgeory
+    csv_name = f"{category_name}.csv"
+    file = open(csv_name, "w", encoding="utf-8", newline="")
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
 
-    # Retrieving HTML content from a category of products pages
-    category_url = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
-
-    current_page_url = category_url
-
     has_next_page = True
-
-    # Retrieving and parsing every product page from a category. And then extracting, transforming, and loading the data of each product page
     while has_next_page:
-        # Download and parse current page
-        category_response = requests.get(current_page_url)
-        category_raw_html = category_response.text
-        category_parsed_html = BeautifulSoup(category_raw_html, "html.parser")
-
-        # Extracting the URL of each product in current page
-        products_blocks =  category_parsed_html.find_all("article", class_="product_pod")
+        # Extracting the URL of each product page
+        products_blocks =  category_parsed.find_all("article", class_="product_pod")
         for product_block in products_blocks:
-            data = product_block.find("a")
-            product_relative_url = data["href"]
-            product_url = urljoin(current_page_url, product_relative_url)
+            product_content = product_block.find("a")
+            product_absolute_url = urljoin(category_absolute_url, product_content["href"])
 
             # Dowload and parse each product page
-            product_response = requests.get(product_url)
-            product_raw_html = product_response.text
-            product_parsed_html = BeautifulSoup(product_raw_html, "html.parser")
+            product_page_parsed = html_parser(product_absolute_url)
             
             # Extracting each "product_page_url"
-            product_page_url = product_url
+            product_page_url = product_absolute_url
 
             # Extracting each "universal_product_code"
-            universal_product_code = extract_table_value(product_parsed_html, "UPC")
+            universal_product_code = extract_table_value(product_page_parsed, "UPC")
 
-            # Extracting each "title"
-            raw_title = product_parsed_html.title.get_text(strip=True)
+            # Extracting and cleaning each "title"
+            raw_title = product_page_parsed.title.get_text(strip=True)
             title = raw_title.replace(" | Books to Scrape - Sandbox", "")
 
             # Extracting and cleaning each "price_including_tax" and "price_excluding_tax" as dictionnaries with values and currencies
                 # Applying to "price_including_tax data"
-            raw_price_including_tax = extract_table_value(product_parsed_html, "Price (incl. tax)")
+            raw_price_including_tax = extract_table_value(product_page_parsed, "Price (incl. tax)")
             price_including_tax_currency = "Inconnue"
             for currency_symbol in CURRENCIES:
                 if currency_symbol in raw_price_including_tax:
@@ -101,13 +124,15 @@ with open("book_data.csv", "w", encoding="utf-8", newline="") as csvfile:
                 .replace("£", "")
                 .strip()
             )
+
             price_including_tax_value = float(cleaned_price_including_tax_text)
+
             price_including_tax = {
                 "value" : price_including_tax_value,
                 "currency" : price_including_tax_currency,
             }
                 # Applying to "price_excluding_tax data"
-            raw_price_excluding_tax = extract_table_value(product_parsed_html, "Price (excl. tax)")
+            raw_price_excluding_tax = extract_table_value(product_page_parsed, "Price (excl. tax)")
             price_excluding_tax_currency = "Inconnue"
             for currency_symbol in CURRENCIES:
                 if currency_symbol in raw_price_excluding_tax:
@@ -120,14 +145,16 @@ with open("book_data.csv", "w", encoding="utf-8", newline="") as csvfile:
                 .replace("£", "")
                 .strip()
             )
+
             price_excluding_tax_value = float(cleaned_price_excluding_tax_text)
+
             price_excluding_tax = {
                 "value" : price_excluding_tax_value,
                 "currency" : price_excluding_tax_currency,
             }
 
             # Extract and clean each "number_available" in order to keep only the value
-            raw_number_available = extract_table_value(product_parsed_html, "Availability")
+            raw_number_available = extract_table_value(product_page_parsed, "Availability")
             digits = []
             for char in raw_number_available:
                 if char.isdigit():
@@ -136,18 +163,26 @@ with open("book_data.csv", "w", encoding="utf-8", newline="") as csvfile:
             number_available = int(cleaned_number_available)
 
             # Extract and clean each "product_description"
-            description_header = product_parsed_html.find("div", id="product_description")
-            product_description = description_header.find_next_sibling("p").get_text()
+            description_header = product_page_parsed.find("div", id="product_description")
+
+            if description_header:
+                next_p = description_header.find_next_sibling("p")
+                if next_p:
+                    product_description = next_p.get_text(strip=True)
+                else:
+                    product_description = ""
+            else:
+                product_content = ""
 
             # Extract and clean each "category"
-            ul = product_parsed_html.find("ul", class_="breadcrumb")
+            ul = product_page_parsed.find("ul", class_="breadcrumb")
             li_items = ul.find_all("li")
             third_li = li_items[2]
             a = third_li.find("a")
             category = a.get_text(strip=True)
 
             # Extract and clean each "review_rating"
-            p = product_parsed_html.find("p", class_="star-rating")
+            p = product_page_parsed.find("p", class_="star-rating")
             class_content = p["class"]
             review_rating_text = class_content[1]
             review_number_mapping = {
@@ -160,29 +195,12 @@ with open("book_data.csv", "w", encoding="utf-8", newline="") as csvfile:
             review_rating = review_number_mapping.get(review_rating_text)
 
             # Extract and clean each "image_url" 
-            div = product_parsed_html.find("div", class_="item active")
+            div = product_page_parsed.find("div", class_="item active")
             img = div.find("img")
             image_relative_url = img["src"]
             image_url = urljoin(product_page_url, image_relative_url)
 
-            # Script control
-            print(
-                f"""
-            ------------------------------
-            Product URL: {product_page_url}
-            UPC: {universal_product_code}
-            Title: {title}
-            Price (incl. tax): {price_including_tax['value']} {price_including_tax['currency']}
-            Price (excl. tax): {price_excluding_tax['value']} {price_excluding_tax['currency']}
-            Available: {number_available}
-            Category: {category}
-            Rating: {review_rating}
-            Image URL: {image_url}
-            ------------------------------
-            """
-            )
-
-        # Build one row and write it immediately
+            # Build one row and write it immediately
             book_data = {
                 "product_page_url": product_page_url,
                 "universal_product_code": universal_product_code,
@@ -198,10 +216,13 @@ with open("book_data.csv", "w", encoding="utf-8", newline="") as csvfile:
             writer.writerow(book_data)
 
         # Find link to next page
-        next_page_data = category_parsed_html.find("li", class_="next")
+        next_page_data = category_parsed.find("li", class_="next")
         if next_page_data:
             next_page_index = next_page_data.find("a")["href"]
-            current_page_url = urljoin(current_page_url, next_page_index)
+            category_absolute_url = urljoin(category_absolute_url, next_page_index)
+            category_parsed = html_parser(category_absolute_url)
         else:
             has_next_page = False
 
+    # CLosing the csv file for a category
+    file.close()
