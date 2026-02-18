747b00c8ad590dba73956d9f846f83a537c4e34c
Armand de la Porte des Vaux <armand.delaporte@gmail.com>
2026-02-18 11:16:12 +0100

    refactor: harmonize naming and harden image download pipeline - standardize variable names for readability - make image download non-blocking - log failures in script and CSV - validate downloaded images with Pillow - update requirements.txt

--- src/main.py
+++ src/main.py
@@ -25,6 +25,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import csv
 from pathlib import Path
+from PIL import Image
 
 # HELPERS
 # Helper to retrieve data from product information header
@@ -78,6 +79,8 @@ fieldnames = [
     "review_rating",
     "image_url",
     "image_local_path",
+    "image_download_status",
+    "image_error"
 ]
 
 # MAIN EXECUTION
@@ -91,10 +94,10 @@ images_folder.mkdir(parents=True, exist_ok=True)
 
 # Download and parse "Books to Scrape" website homepage
 homepage_url = "https://books.toscrape.com/"
-homepage_parsed = html_parser(homepage_url)
+homepage_soup = html_parser(homepage_url)
 
 # Extract and clean categories absolute URLs and names
-categories_sidebar = homepage_parsed.find("div", class_="side_categories")
+categories_sidebar = homepage_soup.find("div", class_="side_categories")
 category_links = categories_sidebar.find_all("a")
 
 category_index = []
@@ -114,8 +117,8 @@ total_images = 0
 
 # Download and parse every category page
 for category in category_index:
-    category_url = category["url"]
-    category_parsed = html_parser(category_url)
+    category_absolute_url = category["url"]
+    category_soup = html_parser(category_absolute_url)
 
     # Extract category name
     category_name = category["name"]
@@ -123,127 +126,150 @@ for category in category_index:
     # Create a specific csv file per catgeory
     csv_path = data_folder / f"{category_name}.csv"
     with open(csv_path, "w", encoding="utf-8", newline="") as csv_file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
 
-    # Iterate over each product card to extract product details
-    has_next_page = True
-    while has_next_page:
-        # Extract the URL of product page
-        product_cards =  category_parsed.find_all("article", class_="product_pod")
-        for card in product_cards:
-            product_link = card.find("a")
-            product_absolute_url = urljoin(category_absolute_url, product_link["href"])
+        # Iterate over each product card to extract product details
+        has_next_page = True
+        while has_next_page:
+            # Extract the URL of product page
+            product_cards =  category_soup.find_all("article", class_="product_pod")
+            for card in product_cards:
+                product_link = card.find("a")
+                product_absolute_url = urljoin(category_absolute_url, product_link["href"])
 
-            # Download and parse product page
-            product_page_parsed = html_parser(product_absolute_url)
-            
-            # Extract "product_page_url"
-            product_page_url = product_absolute_url
+                # Download and parse product page
+                product_page_soup = html_parser(product_absolute_url)
+                
+                # Extract "product_page_url"
+                product_page_url = product_absolute_url
 
-            # Extract "universal_product_code"
-            universal_product_code = extract_table_value(product_page_parsed, "UPC")
+                # Extract "universal_product_code"
+                universal_product_code = extract_table_value(product_page_soup, "UPC")
 
-            # Extract and clean "title"
-            raw_title = product_page_parsed.title.get_text(strip=True)
-            title = raw_title.replace(" | Books to Scrape - Sandbox", "")
+                # Extract and clean "title"
+                raw_title = product_page_soup.title.get_text(strip=True)
+                title = raw_title.replace(" | Books to Scrape - Sandbox", "")
 
-            # Extract and normalize price (value + currency) for inclincluding and excluding tax
-            raw_price_including_tax = extract_table_value(product_page_parsed, "Price (incl. tax)")
-            price_including_tax = parse_price(raw_price_including_tax)
+                # Extract and normalize price (value + currency) for inclincluding and excluding tax
+                raw_price_including_tax = extract_table_value(product_page_soup, "Price (incl. tax)")
+                price_including_tax = parse_price(raw_price_including_tax)
 
-            raw_price_excluding_tax = extract_table_value(product_page_parsed, "Price (excl. tax)")
-            price_excluding_tax = parse_price(raw_price_excluding_tax)
+                raw_price_excluding_tax = extract_table_value(product_page_soup, "Price (excl. tax)")
+                price_excluding_tax = parse_price(raw_price_excluding_tax)
 
-            # Extract and clean "number_available"
-            raw_number_available = extract_table_value(product_page_parsed, "Availability")
-            digits = []
-            for char in raw_number_available:
-                if char.isdigit():
-                    digits.append(char)
-            number_available_str = "".join(digits)
-            number_available = int(number_available_str)
+                # Extract and clean "number_available"
+                raw_number_available = extract_table_value(product_page_soup, "Availability")
+                digits = []
+                for char in raw_number_available:
+                    if char.isdigit():
+                        digits.append(char)
+                number_available_str = "".join(digits)
+                number_available = int(number_available_str)
 
-            # Extract and clean "product_description"
-            description_container = product_page_parsed.find("div", id="product_description")
+                # Extract and clean "product_description"
+                description_container = product_page_soup.find("div", id="product_description")
 
-            if description_container:
-                description_paragraph = description_container.find_next_sibling("p")
-                if description_paragraph:
-                    product_description = description_paragraph.get_text(strip=True)
+                if description_container:
+                    description_paragraph = description_container.find_next_sibling("p")
+                    if description_paragraph:
+                        product_description = description_paragraph.get_text(strip=True)
+                    else:
+                        product_description = ""
                 else:
                     product_description = ""
+
+                # Extract and clean each "review_rating"
+                rating_tag = product_page_soup.find("p", class_="star-rating")
+                rating_tag_classes = rating_tag["class"]
+                rating_text = rating_tag_classes[1]
+                rating_mapping = {
+                    "One" : 1, 
+                    "Two" : 2, 
+                    "Three" : 3, 
+                    "Four" : 4, 
+                    "Five" : 5
+                }
+                review_rating = rating_mapping.get(rating_text)
+
+                # Extract and clean each "image_url" 
+                image_container = product_page_soup.find("div", class_="item active")
+                image_tag = image_container.find("img")
+                image_relative_url = image_tag["src"]
+                image_absolute_url = urljoin(product_page_url, image_relative_url)
+                image_url = image_absolute_url
+
+                # Download images on local folder ouput/images
+                image_filename = f"{universal_product_code}.jpg"
+                image_path = images_folder / image_filename
+                image_error = ""
+                image_download_status = "pending"
+                image_path_for_csv = f"images/{image_filename}"
+
+                try:
+                    response_image_url = requests.get(image_absolute_url, stream=True, timeout=30)
+                    response_image_url.raise_for_status()
+                    
+                    with open(image_path, "wb") as file:
+                        for chunk in response_image_url.iter_content(8192):
+                            if chunk:
+                                file.write(chunk)
+                    
+                    # Validation
+                    try: 
+                        with Image.open(image_path) as image:
+                            image.load()
+                        image_download_status = "successful"
+                        total_images += 1
+                    except Exception:
+                        image_download_status = "failed"
+                        image_error = "Downloaded file is not a valid image"
+                        image_path_for_csv = "none"
+                        image_path.unlink(missing_ok=True)
+
+                    # Adding 1 to the number of exported images for later control
+                    total_images += 1
+                    image_download_status = "successful"
+
+                except requests.exceptions.RequestException as exception_type:
+                    print(f"Erreur téléchargement image : {exception_type}")
+                    image_download_status = "failed"  
+                    image_error = type(exception_type).__name__       
+                    image_path_for_csv = "none"
+
+                # Build one row and write it immediately
+                book_data = {
+                    "product_page_url": product_page_url,
+                    "universal_product_code": universal_product_code,
+                    "title": title,
+                    "price_including_tax": price_including_tax["value"],
+                    "price_excluding_tax": price_excluding_tax["value"],
+                    "number_available": number_available,
+                    "product_description": product_description,
+                    "category": category_name,
+                    "review_rating": review_rating,
+                    "image_url": image_url,
+                    "image_local_path" : image_path_for_csv,
+                    "image_download_status" : image_download_status,
+                    "image_error" : image_error
+                }
+                
+                writer.writerow(book_data)
+
+                # Adding 1 to the number of exported books for later control
+                total_books += 1
+
+            # Find link to next page
+            next_page_tag = category_soup.find("li", class_="next")
+            if next_page_tag:
+                next_page_relative_url = next_page_tag.find("a")["href"]
+                category_absolute_url = urljoin(category_absolute_url, next_page_relative_url)
+                category_soup = html_parser(category_absolute_url)
             else:
-                product_description = ""
+                has_next_page = False
 
-            # Extract and clean each "review_rating"
-            rating_tag = product_page_parsed.find("p", class_="star-rating")
-            rating_tag_classes = rating_tag["class"]
-            rating_text = rating_tag_classes[1]
-            rating_mapping = {
-                "One" : 1, 
-                "Two" : 2, 
-                "Three" : 3, 
-                "Four" : 4, 
-                "Five" : 5
-            }
-            review_rating = rating_mapping.get(rating_text)
-
-            # Extract and clean each "image_url" 
-            image_container = product_page_parsed.find("div", class_="item active")
-            image_tag = image_container.find("img")
-            image_relative_url = image_tag["src"]
-            image_absolute_url = urljoin(product_page_url, image_relative_url)
-            image_url = image_absolute_url
-
-            # Download images on local folder ouput/images
-            image_filename = f"{universal_product_code}.jpg"
-            image_path = images_folder / image_filename
-
-            response_image_url = requests.get(image_absolute_url, stream=True, timeout=30)
-            response_image_url.raise_for_status() 
-
-            with open(image_path, "wb") as file:
-                for chunk in response_image_url.iter_content(8192):
-                    if chunk:
-                        file.write(chunk)
-
-            image_path_for_csv = f"images/{image_filename}"
-            
-            # Adding 1 to the number of exported images for later control
-            total_images += 1
-
-            # Build one row and write it immediately
-            book_data = {
-                "product_page_url": product_page_url,
-                "universal_product_code": universal_product_code,
-                "title": title,
-                "price_including_tax": price_including_tax["value"],
-                "price_excluding_tax": price_excluding_tax["value"],
-                "number_available": number_available,
-                "product_description": product_description,
-                "category": category_name,
-                "review_rating": review_rating,
-                "image_url": image_url,
-                "image_local_path" : image_path_for_csv
-            }
-            writer.writerow(book_data)
-
-            # Adding 1 to the number of exported books for later control
-            total_books += 1
-
-
-        # Find link to next page
-        next_page_data = category_parsed.find("li", class_="next")
-        if next_page_data:
-            next_page_index = next_page_data.find("a")["href"]
-            category_absolute_url = urljoin(category_absolute_url, next_page_index)
-            category_parsed = html_parser(category_absolute_url)
-        else:
-            has_next_page = False
-
-    # CLosing the csv file for a category
-    file.close()
+        # CLosing the csv file for a category
+        csv_file.close()
 
 # Controlling the total number of exported books and images
 print(f"Total number of exported books: {total_books}")
